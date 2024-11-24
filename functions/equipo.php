c9a658afa3005c67037a25100d31cdb0acc52e02
AngelC <angelcoyacmo@gmail.com>
2024-11-24 15:12:24 -0700

    a

--- functions/equipo.php
+++ functions/equipo.php
@@ -5,14 +5,14 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $nombre = $_POST['nombre'];
     $estado = $_POST['estado'];
     $fk_tanque = $_POST['fk_tanque'];
-    $fecha = $_POST['fecha'];
+    
 
     $equipo = new Equipo();
     
-    $registroExitoso = $equipo->insertar($nombre, $estado, $fk_tanque, $fecha);
+    $registroExitoso = $equipo->insertar($nombre, $estado, $fk_tanque);
 
     if ($registroExitoso === true) {
-        header("Location: ../views/panel.php");
+        header("Location: ../views/tabla_equipo.php");
         exit;                               
     } else {
        
